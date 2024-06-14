f1f0d2e897920f2e2910a57f95274be210a54ee2
Євген бабич <evgembabych1658@gmail.com>
2024-06-14 15:36:46 +0300

    finals

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
-require("dotenv");
 const mysql = require('mysql2');
 
-// Використовуємо змінні середовища
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbDatabase = process.env.DB_DATABASE;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
 // Створюємо URI з використанням змінних середовища
-const uri = `mysql://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}`;
+const uri = `mysql://root:kSQyoPdWXvAwQiECXMIWeShvmIGsZcwm@viaduct.proxy.rlwy.net:44561/railway`;
 const connection = mysql.createConnection(uri);
 
 connection.connect(err => {
